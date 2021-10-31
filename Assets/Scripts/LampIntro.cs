672e22efe188b060538a1e3b8ee5d17cd8f59af5
Zemerr <zeemer1997@gmail.com>
2021-11-01 00:30:31 +0200

    Radar effect add

--- Assets/Scripts/LampIntro.cs
+++ Assets/Scripts/LampIntro.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class LampIntro : MonoBehaviour
 {
-	public float speed, deep;
+	private int speed = 45;
+	private int deep = 20;
+	private float timeLeft = 2.5F;
+	[SerializeField] private bool intro;
 	[SerializeField] private Vector3 start;
     [SerializeField] private Vector3 end;
     [SerializeField] private Vector3 target;
+	[SerializeField] GameObject backIntro;
+	[SerializeField] GameObject main;
 
     void Start() {
         start = transform.position;
         end = transform.position;
         end.y += deep;
         target = end;
+		intro = true;
     }
 
 	void Update () {
-		transform.position = Vector3.MoveTowards (transform.position, target, Time.deltaTime * speed);
-		if (transform.position == end)
-			target = start;
-		else if (transform.position == start)
-			target = end;
-
-		// transform.Rotate (Vector3.up * tilt);
+		if (intro)
+		{
+			transform.position = Vector3.MoveTowards (transform.position, target, Time.deltaTime * speed);
+			if (transform.position == end)
+				target = start;
+			else if (transform.position == start)
+				target = end;
+			
+			timeLeft -= Time.deltaTime;
+			if ( timeLeft < 0 )
+			{
+				intro = false;
+				// gameObject.SetActive (false);
+				// introText.SetActive (false);
+				main.SetActive (true);
+				backIntro.SetActive (false);
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/Radar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour {
+
+    private Transform sweepTransform;
+    private float rotationSpeed;
+
+    private void Awake() {
+        sweepTransform = transform.Find("Sweep");
+        rotationSpeed = 180f;
+    }
+
+    private void Update() {
+        float previousRotation = (sweepTransform.eulerAngles.z % 360) - 180;
+        sweepTransform.eulerAngles -= new Vector3(0, 0, rotationSpeed * Time.deltaTime);
+    }
+
+}
