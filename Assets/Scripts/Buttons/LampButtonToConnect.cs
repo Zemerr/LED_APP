e9db37c6c92fbdfecf13de137b0bfde2a2104d3b
Zemerr <zeemer1997@gmail.com>
2021-11-05 14:52:31 +0200

    Lamp  button front aleray finihed

---
+++ Assets/Scripts/Buttons/LampButtonToConnect.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class LampButtonToConnect : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData eventData) {
+        transform.position = new Vector3 (transform.position.x, transform.position.y+6f, transform.position.z);
+    }
+    public void OnPointerUp(PointerEventData eventData) {
+        transform.position = new Vector3 (transform.position.x, transform.position.y-6f, transform.position.z);
+#if UNITY_EDITOR
+        Debug.Log("..........Connection............");
+#endif
+    }
+
+}
---
+++ Assets/Scripts/Buttons/SwichBut.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class SwichBut : MonoBehaviour, IPointerDownHandler
+{
+
+
+    public bool swith = true;
+
+    [SerializeField] private GameObject switcher;
+    [SerializeField] private GameObject leftSide;
+    private Vector3 switchPos;
+    private Vector3 rightSwitchPos;
+    private Vector3 leftSwitchPos;
+    private Color onColor;
+    private Color ofColor;
+
+    void Start() {
+        switchPos = switcher.gameObject.transform.position;
+        rightSwitchPos = switchPos;
+        leftSwitchPos = leftSide.gameObject.transform.position;
+        onColor = new Color32(82,157,209,255);
+        ofColor = new Color32(212,111,120,255);
+
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData) {
+        SwitchOnOf(swith);
+    }
+
+    public void SwitchOnOf(bool state, bool onlyFronSide = false) {
+        if (state) 
+        {
+#if UNITY_EDITOR
+            Debug.Log("..........SWITCH OFF............");
+#endif
+            GetComponent<Image>().color = ofColor;
+            switcher.gameObject.transform.position = leftSwitchPos;
+            swith = false;
+        }
+        else
+        {
+#if UNITY_EDITOR
+            Debug.Log("..........SWITCH ON............");
+#endif
+            GetComponent<Image>().color = onColor;
+            switcher.gameObject.transform.position = rightSwitchPos;
+            swith = true;
+        }
+    }
+}
